b52ed266cf6b4ed916a44af294c7e88b061e7a5c
mmlofficial <47824135+mmlofficial@users.noreply.github.com>
2019-12-14 19:44:52 +0300

    Project with authorization added.

---
+++ CoDA/CoDA/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CoDA.Startup))]
+namespace CoDA
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
